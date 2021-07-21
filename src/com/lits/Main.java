ee858a86abc7f07f98e1fc6ef237f7dc1c1d3725
yarchyk <yarkosvyshch2@gmail.com>
2021-07-21 09:07:25 +0200

    initial commit

---
+++ src/com/lits/Main.java
@@ -0,0 +1,31 @@
+package com.lits;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+/*
+        double b = 125;
+        double b1 = 2;
+        System.out.println("b + b1 =" + (double)(b / b1));
+*/
+        /*
+        System.out.println();
+
+        double b = 124;
+        double b1 = 2;
+        System.out.println("b / b1 =" + (double)(b / b1));
+
+        System.out.println(1L);
+        boolean s = 9%2 == 0;
+        System.out.println(s);
+
+*/
+        var i = 10f
+
+
+    }
+
+}
+
+
